e7695fa55671a47c7f8bf2371c6e8680f1873365
PJL33 <118242467+PJL33@users.noreply.github.com>
2023-09-11 20:39:17 -0400

    9/11/2023 - 8:39

---
+++ competions.js
@@ -0,0 +1,49 @@
+let seehp = document.getElementById("btn-see-hp");
+let hidehp = document.getElementById("btn-hide-hp");
+let hpImg = document.getElementById("hp-img");
+seehp.addEventListener("click",function(){
+    hpImg.style.cssText = "dsplay:block;";
+});
+hidehp.addEventListener("click",function(){
+    hpImg.style.cssText = "display:none;";
+});
+
+let seeEh = document.getElementById("btn-see-eh");
+let hideEh = document.getElementById("btn-hide-eh");
+let ehImg = document.getElementById("eh-img");
+seeEh.addEventListener("click",function(){
+    ehImg.style.cssText = "dsplay:block;";
+});
+hideEh.addEventListener("click",function(){
+    ehImg.style.cssText = "display:none;";
+});
+
+let seeFll = document.getElementById("btn-see-fll");
+let hideFll = document.getElementById("btn-hide-fll");
+let fllImg = document.getElementById("fll-img");
+seeFll.addEventListener("click",function(){
+    fllImg.style.cssText = "dsplay:block;";
+});
+hideFll.addEventListener("click",function(){
+    fllImg.style.cssText = "display:none;";
+});
+
+let seeExsha1 = document.getElementById("btn-see-exsha1");
+let hideExsha1 = document.getElementById("btn-hide-exsha1");
+let exsha1Img = document.getElementById("exsha1-img");
+seeExsha1.addEventListener("click",function(){
+    exsha1Img.style.cssText = "dsplay:block;";
+});
+hideExsha1.addEventListener("click",function(){
+    exsha1Img.style.cssText = "display:none;";
+});
+
+let seeFlll = document.getElementById("btn-see-flll");
+let hideFlll = document.getElementById("btn-hide-flll");
+let flllImg = document.getElementById("flll-img");
+seeFlll.addEventListener("click",function(){
+    flllImg.style.cssText = "dsplay:block;";
+});
+hideFlll.addEventListener("click",function(){
+    flllImg.style.cssText = "display:none;";
+});
