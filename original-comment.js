e5df7fa821f4001e8869b7839422bedf6a24f114
PJL33 <118242467+PJL33@users.noreply.github.com>
2023-09-17 14:05:50 -0400

    9/17/2023 - 2:05 PM

--- original-comment.js
+++ original-comment.js
@@ -167,15 +167,172 @@ talie.addEventListener("click",function(){
         }  
 })
 
-// let talie = document.getElementById("talie")
-// talie.addEventListener("click",function(){
-//     let length = talie.innerText.length;
-//     if(length ==17){
-//         talie.innerText = 'Back to the translated version';
-//         talie.parentNode.parentNode.getElementsByTagName("p")[0].innerText = "orignal version stevenson"; //sa moun nan ekri an
-//         }
-//     else{
-//         talie.innerText = "See original text";
-//         talie.parentNode.parentNode.getElementsByTagName("p")[0].innerText = "translated version Stevenson"; //vesyon kem tradwi an
-//         }  
-// })
+// Where the carousel for the mobile phones begins
+let dimitry2 = document.getElementById("dimitry2")
+dimitry2.addEventListener("click",function(){
+    let length = dimitry2.innerText.length;
+    if(length ==17){
+        dimitry2.innerText = 'Back to the translated version';
+        dimitry2.parentNode.parentNode.getElementsByTagName("p")[0].innerText = "Mwen panse Johndy se yon jèn trè dinamik epi ki inovatè nan ide l parapò ak pwojè li konn ap prezante ban m. M panse li kreyatif tou epi ap travay chak jou pou l plis konsistan."; //sa moun nan ekri an
+        }
+    else{
+        dimitry2.innerText = "See original text";
+        dimitry2.parentNode.parentNode.getElementsByTagName("p")[0].innerText = "I think Johndy is a very dynamic and innovative young man in his ideas regarding the projects he usually presents to me. I think he is also creative and works every day to be more consistent."; //vesyon kem tradwi an
+        }  
+})
+
+let maurice2 = document.getElementById("maurice2")
+maurice2.addEventListener("click",function(){
+    let length = maurice2.innerText.length;
+    if(length ==17){
+        maurice2.innerText = 'Back to the translated version';
+        maurice2.parentNode.parentNode.getElementsByTagName("p")[0].innerHTML = ".JOHNDY était le président de ma classe de philo. Cela ne définit en rien sa personne, mais je tenais à le souligner, car il faut être en mesure de convaincre une salle de classe et gagner sa confiance pour être président en philo. Il est un travailleur acharné, avec de grands rêves et des projets plein la tête. C'est quelqu'un d'une grande intelligence.<br>Ce n'est pas quelqu'un qui se sent totalement à l'aise en public ou au sein d'un groupe. Parfois, j'ai l'impression qu'il fait beaucoup d'efforts pour dissimuler son manque d'aisance(très peu de gens le remarque). Il est cependant un bon sportif et un excellent apprenant. Je dis cela non seulement en raison de ses notes en classe secondaire, mais surtout en raison de son enthousiasme pour apprendre et se surpasser.<br>C'est quelqu'un de mature, et je tiens à souligner que sa maturité découle de ses expériences. À l'école classique, il faisait preuve d'une capacité de réflexion à laquelle très peu de jeunes pouvaient consacrer le temps nécessaire. Sa maturité lui permettait de gérer les activités avec une attention aux détails que les autres prêtaient rarement. Il est d'une franchise rare, disant ce qu'il pense, et il était souvent une source de bons conseils (Ne change en rien cet aspect de ta personnalité).<br>Ce n'est pas un secret pour ceux qui ont un jour côtoyé JOHNDY : il est extrêmement intelligent et mature. On a l'impression qu'il se sent seul, incompris. Il travaille dur à la réalisation de ses rêves et projets au point d'oublier de dormir. J'ai des souvenirs de JOHNDY épuisé, s'endormant en classe avec les yeux fatigués et rouges.<br>Un conseil pour toi, JOHNDY : relâche un peu la pression. Je ne te dis pas d'oublier tes rêves ou de les abandonner, mais plutôt de prendre soin de toi en dormant davantage pour maintenir ta santé et vivre une vie plus longue, mon ami. Je crois en toi, et je sais que tu as le potentiel pour réaliser tous tes rêves. N'oublie pas que le repos et la détente sont tout aussi importants"; //sa moun nan ekri an
+        }
+    else{
+        maurice2.innerText = "See original text";
+        maurice2.parentNode.parentNode.getElementsByTagName("p")[0].innerHTML = "JOHNDY was the president of my 12th grade class. This does not define that person, but I wanted to emphasize it, because you have to be able to convince a classroom and gain that confidence to be the president of a family. He is a hard worker, with big dreams and projects in his head. He is someone of great intelligence.<br> He is not someone who feels completely comfortable in public or in a group. Sometimes I feel like he goes to great lengths to hide his lack of comfort (very few people notice). He is, however, very sportive and an excellent learner. I say this not only because of his grades in high school, but especially because of his enthusiasm to learn and surpass himself.<br>He is a mature person, and I want to emphasize that his maturity comes from his experiences. At classical school, he demonstrated a capacity for reflection to which very few young people could devote the necessary time. His maturity allowed him to manage activities with attention to detail that others rarely paid. He is unusually frank, saying what he thinks, and he was often a source of good advice (Don't change this aspect of your personality).<br>It's no secret for those who have ever been around JOHNDY: he is extremely intelligent and mature. We have the impression that he feels alone, misunderstood. He works hard to achieve his dreams and projects to the point of forgetting to sleep. I have memories of JOHNDY exhausted, falling asleep in class with tired, red eyes.<br>A word of advice for you, JOHNDY: take the pressure off a little. I'm not telling you to forget your dreams or give up on them, but rather take care of yourself by getting more sleep to maintain your health and live a longer life, my friend. I believe in you, and I know that you have the potential to achieve all your dreams. Remember that rest and relaxation are equally important."; //vesyon kem tradwi an
+        }  
+})
+
+let mickaello2 = document.getElementById("mickaello2")
+mickaello2.addEventListener("click",function(){
+    let length = mickaello2.innerText.length;
+    if(length ==17){
+        mickaello2.innerText = 'Back to the translated version';
+        mickaello2.parentNode.parentNode.getElementsByTagName("p")[0].innerText = "Mwen panse Johndy se yon entrovèti ki entelijan . Li enterese e motive anpil poul aprann nouvo bagay chak jou kap pase . Li renmen analize obsève anvan li aji . Souvan fason li entrovèti a konn pouse l a elwanye de moun kap vinn kote l sa ki konn rann rapól nan milye sosyal difisil men aprèsa sa Li se yon moun ki motive pou atenn objectif li"; //sa moun nan ekri an
+        }
+    else{
+        mickaello2.innerText = "See original text";
+        mickaello2.parentNode.parentNode.getElementsByTagName("p")[0].innerText = "I think Johndy is a smart introvert. He is very interested and motivated to learn new things every day. He likes to analyze and observe before he acts. Often his introverted side pushes him away from people who come to him, which makes his rapport in a social environment difficult, but after that he is a motivated person to reach his goal."; //vesyon kem tradwi an
+        }  
+})
+
+let redjina2 = document.getElementById("redjina2")
+redjina2.addEventListener("click",function(){
+    let length = redjina2.innerText.length;
+    if(length ==17){
+        redjina2.innerText = 'Back to the translated version';
+        redjina2.parentNode.parentNode.getElementsByTagName("p")[0].innerText = "Johndy se yon moun responsab ki toujou prè pou mete kè li nan tout sa lap fè epi ede moun et ceci menm si li pap benefisye anyen ni gen okenn enterè an retou."; //sa moun nan ekri an
+        }
+    else{
+        redjina2.innerText = "See original text";
+        redjina2.parentNode.parentNode.getElementsByTagName("p")[0].innerText = "Johndy is a responsible person who is always ready to put his heart into everything he does and help people even if he will not benefit anything or have any interest in return."; //vesyon kem tradwi an
+        }  
+})
+
+let xiomara2 = document.getElementById("xiomara2")
+xiomara2.addEventListener("click",function(){
+    let length = xiomara2.innerText.length;
+    if(length ==17){
+        xiomara2.innerText = 'Back to the translated version';
+        xiomara2.parentNode.parentNode.getElementsByTagName("p")[0].innerHTML = "Kisa m panse de Johndy?<br>Johndy se yon moun ki souvan apa, ki pa renmen pale si se pa selman poul di on bon bagay...se yon moun ki trè kolaboratif... menmsi bagay la ta deranjew lap diw li...se moun ki obsève byen e anpil...se moun ki gen on bòn kapasite pou koute...se moun ki inovan anpil men konsa tou se on moun ke yo pa renmen fason li frèt la ou ta di li pa sou moun ditou...m ka di ke anbisyon pa konn twòp men li souvan kite pa l yo monte l nan tèt li...se yon moun tou ki enprevizib se pa yon defo ni yon kalite tou...se sa kem ka di sou Jonhdy"; //sa moun nan ekri an
+        }
+    else{
+        xiomara2.innerText = "See original text";
+        xiomara2.parentNode.parentNode.getElementsByTagName("p")[0].innerHTML = "What do I think of Johndy?<br> Johndy is a person who is often aloof, who doesn't like to talk unless it's just to say something good... He's a very collaborative person... Even if something might disturb you, he'll not be afraid to tell you that... He's a person who observes well and frequently... He has a good ability to listen... He is a very innovative person, but there are also some people who doesn't like the way he is cold, you would say sometimes that he is not interest in people at all... I can say that ambition is never too much but he often lets his own get it in his head. He is also a person who is unpredictable, it is not a flaw nor a quality... That's what I can say about Jonhdy."; //vesyon kem tradwi an
+        }  
+})
+
+let montuma2 = document.getElementById("montuma2")
+montuma2.addEventListener("click",function(){
+    let length = montuma2.innerText.length;
+    if(length ==17){
+        montuma2.innerText = 'Back to the translated version';
+        montuma2.parentNode.parentNode.getElementsByTagName("p")[0].innerText = "Se yon bon nèg ki toujou prè pou ede moun ki kwè nan travay "; //sa moun nan ekri an
+        }
+    else{
+        montuma2.innerText = "See original text";
+        montuma2.parentNode.parentNode.getElementsByTagName("p")[0].innerText = "He is a good guy who is always ready to help people who believe in work"; //vesyon kem tradwi an
+        }  
+})
+
+let samy2 = document.getElementById("samy2");
+samy2.addEventListener("click",function(){
+    let length = samy2.innerText.length;
+    if(length ==17){
+        samy2.innerText = 'Back to the translated version';
+        samy2.parentNode.parentNode.getElementsByTagName("p")[0].innerText = "Mwen panse ke Johndy se yon moun ki gen kouraj, ki renmen bagay ki gen anpil difikilte pou l t ka wè oubyen depase limit li. Li se yon moun ki avize e ki konprann moun men dèfwa li vrèman ka agase moun, lolll. "; //sa moun nan ekri an
+        }
+    else{
+        samy2.innerText = "See original text";
+        samy2.parentNode.parentNode.getElementsByTagName("p")[0].innerText = "I think that Johndy is a brave person, who likes things that are very difficult to see or exceed his limits. He is a wise and understanding person but sometimes he can really annoy people, lol."; //vesyon kem tradwi an
+        }  
+})
+
+let kedar2 = document.getElementById("kedar2")
+kedar2.addEventListener("click",function(){
+    let length = kedar2.innerText.length;
+    if(length ==17){
+        kedar2.innerText = 'Back to the translated version';
+        kedar2.parentNode.parentNode.getElementsByTagName("p")[0].innerText = "Dabò Johndy se yon moun ki gen anpil odas, e ki pa pè batay Pou sa Li vle. Li trè konsantre, e entelijan anpil. Li renmen ankouraje moun Nan antouraj Li, e Li mete l disponib Pou moun ki pwòch Li. Li gen tandans pèsiste menm lè yo di l non, Li gen yon kote entwovèti kote gen de lè Li rete Li pa sou bò moun ditou. Epi Li pa renmen pèdi, pafwa sa konn fè l konpòte l bizaman, li gen yon kote kolerik Tou, sa fè lè l fache Li kategorik ak pawòl li. Li gen yon sans obsèvasyon pouse, e Li atansyone anpil. Li gen yon kote blagè Tou, epi Li byen renmen admire bèl nègès byen kanpe. Li se yon moun ki gen pasyans, e l se yon bon konseye."; //sa moun nan ekri an
+        }
+    else{
+        kedar2.innerText = "See original text";
+        kedar2.parentNode.parentNode.getElementsByTagName("p")[0].innerText = "First of all, Johndy is a very daring man, and who is not afraid to fight for what He wants. He is very focused, and very intelligent. He likes to encourage people In His surroundings, and He is made available to those close to Him. He tends to persist even when they say no, He has an introverted side where sometimes He remains away from people. Moreover, He doesn't like to lose, sometimes it makes him behave mischievously. when he is angry he is categorical with his words. He has a keen sense of observation, and He is very attentive. He has a side of humor too, and He likes to admire beautiful girls. He is patient and a good counselor."; //vesyon kem tradwi an
+        } 
+})
+
+let schnell2 = document.getElementById("schnell2")
+schnell2.addEventListener("click",function(){
+    let length = schnell2.innerText.length;
+    if(length ==17){
+        schnell2.innerText = 'Back to the translated version';
+        schnell2.parentNode.parentNode.getElementsByTagName("p")[0].innerText = "Johndy se yon nèg konpetan, siw pa konn l vrèman wap panse li se granpapa yon GOAT (lol) paske se enèji sa li degaje men li se yon GOAT, sa nou klè sou sa. Se youn nan nèg m panse si wap vire avèl e kew gen anbisyon san pale menm de ego wap toujou sou travay menm si se pa menm entansite ak li. "; //sa moun nan ekri an
+        }
+    else{
+        schnell2.innerText = "See original text";
+        schnell2.parentNode.parentNode.getElementsByTagName("p")[0].innerText = "Johndy is a competent guy, if you don't really know him you will think he is the grandfather of a GOAT (lol) because of the energy he releases but he is a GOAT, we are clear about that. He's one of the guys I think if you're hanging out with him and have ambition without even talking about ego, you'll always feel motivated to work even if it's not in the same intensity as him."; //vesyon kem tradwi an
+        }  
+})
+
+let dachca2 = document.getElementById("dachca2")
+dachca2.addEventListener("click",function(){
+    let length = dachca2.innerText.length;
+    if(length ==17){
+        dachca2.innerText = 'Back to the translated version';
+        dachca2.parentNode.parentNode.getElementsByTagName("p")[0].innerText = "Il est un jeune homme responsable,  très déterminé. Il est gentil, c'est un très bon conseiller.  C'est quelqu'un en qui on peut placer sa confiance.  Il n'est pas parfait mais il fait des efforts pour cacher ses imperfections."; //sa moun nan ekri an
+        }
+    else{
+        dachca2.innerText = "See original text";
+        dachca2.parentNode.parentNode.getElementsByTagName("p")[0].innerText = "He is a responsible young man, very determined. He is kind, he is a very good advisor. He is someone you can place your trust in. He's not perfect but he tries hard to hide his imperfections."; //vesyon kem tradwi an
+        }  
+})
+
+let falandy2 = document.getElementById("falandy2")
+falandy2.addEventListener("click",function(){
+    let length = falandy2.innerText.length;
+    if(length ==17){
+        falandy2.innerText = 'Back to the translated version';
+        falandy2.parentNode.parentNode.getElementsByTagName("p")[0].innerText = "Mwen panse ou se yon jèn kib tré disipline,boure ak talan, ak anpil vizyon ,sitou nan sa ki gen rapo ak etid ou,se la kem konnenw tou, pwojè an se yon prèv konkrè de moun ke mwen panse ke ou ye an,e mwen nan sam pi fo se asimile sak bon kay lot moun e felisitel tou,donk pa etone swi wew lanse yon pwojè parey 😂👌,kontinye sou menm lanse an frem koz peyi an ap bezwen nou."; //sa moun nan ekri an
+        }
+    else{
+        falandy2.innerText = "See original text";
+        falandy2.parentNode.parentNode.getElementsByTagName("p")[0].innerText = "I think you are a very disciplined young person, full of talent, with a lot of vision, especially in what is related to your studies, that's where I know, the project is a concrete proof of the person that I think you are, and I'm at my best in assimilating the good things of other people and congratulating them too, so don't be surprised if you see I launch a project like this 😂👌, keep on launching the same because Haiti will need us."; //vesyon kem tradwi an
+        }  
+})
+
+let isaac2 = document.getElementById("isaac2")
+isaac2.addEventListener("click",function(){
+    let length = isaac2.innerText.length;
+    if(length ==17){
+        isaac2.innerText = 'Back to the translated version';
+        isaac2.parentNode.parentNode.getElementsByTagName("p")[0].innerText = "Un ami, compréhensif, patient, honnête, perfectioniste, entrepreneur, ponctuel, loyal, laborieux, studieux, attentif, entrepreneur, un leader , un modèle,un frère."; //sa moun nan ekri an
+        }
+    else{
+        isaac2.innerText = "See original text";
+        isaac2.parentNode.parentNode.getElementsByTagName("p")[0].innerText = "A friend, comprehensive, patient, honest, perfectionist, entrepreneur, punctual, loyal, hardworking, studious, attentive, entrepreneur, a leader, a model, a brother."; //vesyon kem tradwi an
+        }  
+})
+
+let talie2 = document.getElementById("talie2")
+talie2.addEventListener("click",function(){
+    let length = talie2.innerText.length;
+    if(length ==17){
+        talie2.innerText = 'Back to the translated version';
+        talie2.parentNode.parentNode.getElementsByTagName("p")[0].innerHTML = "'Détermination, discipline, courage' : ces trois mots pour décrire Johndy Philistin. Il incarne ces qualités de manière exceptionnelle, ce qui le rend vraiment unique. Johndy est toujours prêt à relever les défis qui se présentent à lui.<br>Ce qui m'impressionne le plus chez lui, c'est son leadership inégalé et sa capacité à inspirer les autres, même dans les moindres détails. Avoir Johndy dans son entourage est une source constante de motivation. Son dévouement au travail est remarquable, et il sait comment susciter l'enthousiasme chez ceux qui l'entourent pour atteindre des objectifs communs."; //sa moun nan ekri an
+        }
+    else{
+        talie2.innerText = "See original text";
+        talie2.parentNode.parentNode.getElementsByTagName("p")[0].innerHTML = '"Determination, discipline, courage": these three words to describe Johndy Philistin. He embodies these qualities in an exceptional way, which makes him truly unique. Johndy is always ready to take on the challenges that come his way.<br>What impresses me most about him is his unparalleled leadership and ability to inspire others, even in the smallest details. Having Johnny around is a constant source of motivation. His dedication to work is remarkable, and he knows how to generate enthusiasm in those around him to achieve common goals.' ; //vesyon kem tradwi an
+        }  
+})
